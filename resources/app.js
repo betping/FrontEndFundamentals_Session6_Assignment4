83792e27df929e767f2273fa9ecef6f978ba8afc
betping <betping1993@gmail.com>
2017-03-31 18:17:34 +0530

    Add files via upload

---
+++ resources/app.js
@@ -0,0 +1,15 @@
+var sum = 0;
+var i = 1;
+var j = 1000;
+for(i; i<j; i++){
+	sum = sum + i;
+}
+console.log("The Sum of First 1000 Numbers using for loop is="+sum);
+
+while(i < j){
+	sum = sum + i;
+	i++;
+}
+console.log("The Sum of First 1000 Numbers using while loop is="+sum);
+Contact GitHub API Training Shop Blog About
+© 2017 GitHub, Inc. Terms Privacy Security Status Help
